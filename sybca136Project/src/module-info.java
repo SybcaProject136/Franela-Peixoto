14aa0412ac38203ef801173924cafbb0935f508e
SYBCA_Vishal <Vishal07dz2020@gmail.com>
2024-01-08 09:47:15 +0530

    first github commit

---
+++ sybca136Project/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module sybca136Project {
+}
---
+++ sybca136Project/src/sybca136Project/classDemo.java
@@ -0,0 +1,10 @@
+package sybca136Project;
+
+public class classDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("hello world");
+	}
+}
