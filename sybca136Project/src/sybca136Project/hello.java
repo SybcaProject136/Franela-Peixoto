47cc81260ea56c7d0e1c8b5206a52a80cfd23c47
Franela Peixoto <franela0408@gmail.com>
2024-01-15 09:54:12 +0530

    First Message

---
+++ sybca136Project/src/sybca136Project/hello.java
@@ -0,0 +1,11 @@
+package sybca136Project;
+
+public class hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World 1");
+
+	}
+
+}
